72b962b15f518cf13143774da4070b9f4098a248
Bob Grim <grimb300@hotmail.com>
2019-02-26 18:49:25 +0100

    explore scope variables

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,15 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
+  $scope.playlists = [
+    { title: 'test value', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+
   // Form data for the login modal
   $scope.loginData = {};
 
@@ -43,14 +52,16 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
+    { title: 'array object title property defined the second time', id: 1 },
     { title: 'Chill', id: 2 },
     { title: 'Dubstep', id: 3 },
     { title: 'Indie', id: 4 },
     { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Cowbell', id: 6 },
+    { title: 'new data from controller', id: 7 }
   ];
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+  $scope.dynamicViewTitleFromStateParam = $stateParams.id;
 });
